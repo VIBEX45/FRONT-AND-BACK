8bd52fbd3ab1e8638612914f553402b89b169e1b
VIBEX45 <fberahim2@gmail.com>
2024-05-29 14:03:01 +0800

    update tampilan pemesanan lt 1 dan 2

--- lib/User/dashboard_page.dart
+++ lib/User/dashboard_page.dart
@@ -19,6 +19,11 @@ class _DashboardPageState extends State<DashboardPage> {
 
   Color warna1 = Colors.brown.shade200;
   Color warna2 = Colors.brown;
+
+  bool pilih1 = true;
+  bool pilih2 = false;
+  double tinggilt1 = 40;
+  double tinggilt2 = 40;
   
   @override
   Widget build(BuildContext context) {
@@ -177,38 +182,61 @@ class _DashboardPageState extends State<DashboardPage> {
                         )
                       ),
                     ),
-                  ],
-                ),
+                    
                     Container(
                        alignment: Alignment.bottomCenter,
                       // padding: const EdgeInsets.only(bottom: 35),
-                      height: MediaQuery.of(context).size.height *0.82,
+                      height: MediaQuery.of(context).size.height*0.4,
                       width: MediaQuery.of(context).size.width,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
+                          const SizedBox(
+                            height: 0,
+                          ),
                           MaterialButton(
                             onPressed: () {
-                              setState(() {
+                              if (pilih1 == false) {
+                                setState(() {
                                 Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => 
-                                PemesananltPage(admin: true, lantaikamar: true,),
-                                ),
-                              );
-                              });
+                                  context,
+                                    MaterialPageRoute(
+                                      builder: (context) => 
+                                      PemesananltPage(admin: true, lantaikamar: true,),
+                                    ),
+                                  );
+                                });
+
+                              } if (pilih1 == true) {
+                                setState(() {
+                                  tinggilt1 = 140;
+                                  tinggilt2 = 40;
+                                  pilih1 = false;
+                                  pilih2 = true;
+                                });
+
+                              } else {
+                                tinggilt1 = 40;
+                                tinggilt2 = 40;
+                                pilih2 = true;
+                                pilih1 = true;
+                              }
+                              
                             },
                           shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(40.0) ),
                           child: AnimatedContainer(
                               duration: const Duration(milliseconds: 200),
-                              height: 105,
-                              width: 125,
+                              alignment: Alignment.bottomLeft,
+                              padding: const EdgeInsets.only(left: 20, bottom: 5),
+                              
+                              height: tinggilt1,
+                              width: MediaQuery.of(context).size.width,
                               decoration: BoxDecoration(
                                 image: const DecorationImage(
                                   image: AssetImage('lib/src/images/2.jpeg'), 
                                   fit: BoxFit.cover
                                   ),
+                                border: Border.all(color: Colors.white , width: 3),
                                 borderRadius: BorderRadius.circular(20),
                                 boxShadow: [
                                   BoxShadow(
@@ -218,55 +246,76 @@ class _DashboardPageState extends State<DashboardPage> {
                                   ),
                                 ],
                               ),
-                              child: Padding(
-                                padding: const EdgeInsets.only(top: 70, left: 20),
-                                child: Text(
-                                  'Lt.1',
-                                  style: TextStyle(fontSize: 20, 
-                                  fontWeight: FontWeight.bold, 
-                                  color: Colors.white, 
-                                  shadows: [
-                                    Shadow( // bottomLeft
-                                      offset: const Offset(-1.5, -1.5),
-                                      color: warna2
-                                    ),
-                                    Shadow( // bottomRight
-                                      offset: const Offset(1.5, -1.5),
-                                      color: warna2
-                                    ),
-                                    Shadow( // topRight
-                                      offset: const Offset(1.5, 1.5),
-                                      color: warna2
-                                    ),
-                                    Shadow( // topLeft
-                                      offset: const Offset(-1.5, 1.5),
-                                      color: warna2
-                                    ),
-                                  ]),
-                                ),
+                              child: Text(
+                                'Lt.1',
+                                style: TextStyle(fontSize: 20, 
+                                fontWeight: FontWeight.bold, 
+                                color: Colors.white, 
+                                shadows: [
+                                  Shadow( // bottomLeft
+                                    offset: const Offset(-1.5, -1.5),
+                                    color: warna2
+                                  ),
+                                  Shadow( // bottomRight
+                                    offset: const Offset(1.5, -1.5),
+                                    color: warna2
+                                  ),
+                                  Shadow( // topRight
+                                    offset: const Offset(1.5, 1.5),
+                                    color: warna2
+                                  ),
+                                  Shadow( // topLeft
+                                    offset: const Offset(-1.5, 1.5),
+                                    color: warna2
+                                  ),
+                                ]),
                               ),
                             ),
                           ),
+                          const SizedBox(
+                            height: 11,
+                          ),
                           MaterialButton(
                               onPressed: () {
-                              Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => 
-                                PemesananltPage(admin: true, lantaikamar: false,),
-                                ),
-                              );
+                              if (pilih2 == false) {
+                                setState(() {
+                                Navigator.push(
+                                  context,
+                                    MaterialPageRoute(
+                                      builder: (context) => 
+                                      PemesananltPage(admin: true, lantaikamar: false,),
+                                    ),
+                                  );
+                                });
+                              } if (pilih2 == true) {
+                                setState(() {
+                                  tinggilt1 = 40;
+                                  tinggilt2 = 140;
+                                  pilih2 = false;
+                                  pilih1 = true;
+                                  
+                                });
+                              } else {
+                                tinggilt1 = 40;
+                                tinggilt2 = 40;
+                                pilih2 = true;
+                                pilih1 = true;
+                              }
                               },
                             shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(40.0) ),
                               child: AnimatedContainer(
+                                  alignment: Alignment.bottomLeft,
+                                  padding: EdgeInsets.only(left: 20, bottom: 5),
                                   duration: const Duration(milliseconds: 200),
-                                  height: 105,
-                                  width: 125,
+                                  height: tinggilt2,
+                                  width: MediaQuery.of(context).size.width,
                                   decoration: BoxDecoration(
                                     image: const DecorationImage(
                                       image: AssetImage('lib/src/images/1.jpeg'), 
                                       fit: BoxFit.cover
                                       ),
+                                      
+                                    border: Border.all(color: Colors.white , width: 3),
                                     borderRadius: BorderRadius.circular(20),
                                     boxShadow: [
                                       BoxShadow(
@@ -276,38 +325,41 @@ class _DashboardPageState extends State<DashboardPage> {
                                       ),
                                     ],
                                   ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(top: 70, left: 20),
-                                    child: Text(
-                                      'Lt.2',
-                                      style: TextStyle(fontSize: 20, 
-                                      fontWeight: FontWeight.bold, 
-                                      color: Colors.white, 
-                                      shadows: [
-                                        Shadow( // bottomLeft
-                                          offset: const Offset(-1.5, -1.5),
-                                          color: warna2
-                                        ),
-                                        Shadow( // bottomRight
-                                          offset: const Offset(1.5, -1.5),
-                                          color: warna2
-                                        ),
-                                        Shadow( // topRight
-                                          offset: const Offset(1.5, 1.5),
-                                          color: warna2
-                                        ),
-                                        Shadow( // topLeft
-                                          offset: const Offset(-1.5, 1.5),
-                                          color: warna2
-                                        ),
-                                      ]),
-                                    ),
+                                  child: Text(
+                                    'Lt.2',
+                                    style: TextStyle(fontSize: 20, 
+                                    fontWeight: FontWeight.bold, 
+                                    color: Colors.white, 
+                                    shadows: [
+                                      Shadow( // bottomLeft
+                                        offset: const Offset(-1.5, -1.5),
+                                        color: warna2
+                                      ),
+                                      Shadow( // bottomRight
+                                        offset: const Offset(1.5, -1.5),
+                                        color: warna2
+                                      ),
+                                      Shadow( // topRight
+                                        offset: const Offset(1.5, 1.5),
+                                        color: warna2
+                                      ),
+                                      Shadow( // topLeft
+                                        offset: const Offset(-1.5, 1.5),
+                                        color: warna2
+                                      ),
+                                    ]),
                                   ),
                               ),
                             ),
+                        const SizedBox(
+                          height: 40,
+                        )
+                        
                         ],
                       ),
                     ),
+                  ],
+                ),
               ],
             ),
       ],
